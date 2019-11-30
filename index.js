be76bf8be913c8ac746ac571bf8ead379b8bcfec
antonkluev <dev@antonkluev.com>
2019-11-30 15:31:43 +0100

    bias implemented

--- index.js
+++ index.js
@@ -9,17 +9,6 @@
 // 	ct.drawImage(image, 33, 71)
 // })
 
-let createMatrix = (width, height) => {
-	let matrix = []
-	for (let y = 0; y < height; y ++) {
-		if (!matrix[y]) matrix[y] = []
-		for (let x = 0; x < width; x ++) {
-			matrix[y][x] = Math.random()
-		}
-	}
-	return matrix
-}
-
 let activationFunctions = {
 	sigmoid : {
 		f : x => 1 / (1 + Math.E ** -x), 
@@ -30,6 +19,32 @@ let activationFunctions = {
 		i : x => x
 	}
 }
+
+let multiply = (a, b) => {
+	if (a[0].length != b.length) 
+		return console.error('matrix dimensions do not agree')
+	let c = []
+	for (let y = 0; y < a.length; y ++) {
+		if (!c[y]) c[y] = []
+		for (let x = 0; x < b[0].length; x ++) {
+			if (!c[y][x]) c[y][x] = 0
+			for (let k = 0; k < a[0].length; k ++) {
+				c[y][x] += a[y][k] * b[k][x]
+			}
+		}
+	}
+	return c
+}
+
+let createMatrix = (width, height) => {
+	let matrix = []
+	for (let y = 0; y < height; y ++) {
+		if (!matrix[y]) matrix[y] = []
+		for (let x = 0; x < width; x ++)
+			matrix[y][x] = Math.round(Math.random() * 100) / 100
+	}
+	return matrix
+}
 	
 let initWeightMatricies = layers => {
 	let weights = []
@@ -38,28 +53,52 @@ let initWeightMatricies = layers => {
 	return weights
 }
 
+let initBiases = layers => {
+	let out = []
+	for (let i = 0; i < layers.length - 1; i ++) {
+		if (!out[i]) out[i] = [[]]
+		for (let n = 0; n < layers[i + 1]; n ++)
+			out[i][0][n] = Math.round(Math.random() * 100) / 100
+	}
+	return out
+}
+
 let applyActivationFunction = (input, activationFunction) => {
 	output = [[]]
-	for (let i = 0; i < input[0].length; i ++) {
+	for (let i = 0; i < input[0].length; i ++)
 		output[0][i] = 
 			activationFunctions[activationFunction]
 			.f(input[0][i])
-	}
 	return output
+}
+
+let applyBias = (temp, bias) => {
+	if (temp[0].length != bias[0].length)
+		return console.error('bias dimensions do not agree')
+	for (let i = 0; i < temp[0].length; i ++)
+		temp[0][i] += bias[0][i]
+	return temp
 }
 
 let ANN = (layers, labels, activationFunction) => {
 	let weightMatricies = initWeightMatricies(layers)
+	console.log('weightMatricies', weightMatricies)
+	let biases = initBiases(layers)
+	console.log('biases', biases)
 	return {
 		forwardPropagation (input) {
 			if (layers[0] != input[0].length)
 				return console.error('input dimensions do not agree')
-			cur_input = input
+			temp = input
 			for (let i = 0; i < weightMatricies.length; i++) {
-				cur_input = multiply(cur_input, weightMatricies[i])
-				cur_input = applyActivationFunction(
-					cur_input, 'sigmoid')
+				temp = multiply(temp, weightMatricies[i])
+				temp = applyBias(temp, biases[i])
+				temp = applyActivationFunction(temp, 'linear')
 			}
+			return temp
+		},
+		calculateError () {
+			
 		},
 		backPropagation () {
 			
@@ -68,7 +107,11 @@ let ANN = (layers, labels, activationFunction) => {
 }
 
 // TODO: specify activation function for each layer
-let ann = ANN([2, 3], ['cat', 'dog'])
+let ann = ANN([2, 3, 2], ['cat', 'dog'])
+
+console.log(ann.forwardPropagation([[1, 2]]))
+
+// console.log(initBiases([2, 3, 2]))
 
 // TODO: randomly change weights during computations
 // TODO: does it make the network more robust if we remove random or specific
